7b57c7169b3f0312fc8f12296a0136628b099a82
saugatdhakal <saugatdhakal5@gmail.com>
2025-03-07 19:10:16 +0545

    add sendMessage function import to message route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import {getUserForSidebar,getMessages} from '../controllers/message.controller.js';
+import {getUserForSidebar,getMessages,sendMessage} from '../controllers/message.controller.js';
 
 const router = express.Router();
 
