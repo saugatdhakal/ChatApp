82f63ccaede108f3dd6cac9553ffe0a3b9eab1ae
saugatdhakal <saugatdhakal5@gmail.com>
2025-03-16 17:15:30 +0545

    implement chat container; add chat header and message input components; fetch messages based on selected user

--- frontend/src/Components/ChatContainer.jsx
+++ frontend/src/Components/ChatContainer.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { useChatStore } from "../../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+
 const ChatContainer = () => {
-  return <div>Chat Container</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading) return <div>Loading..</div>;
+
+  return <div className="flex-1 flex flex-col overflow-auto">
+    <ChatHeader/>
+    <p>messages</p>
+    <MessageInput/>
+  </div>;
 };
 export default ChatContainer;
